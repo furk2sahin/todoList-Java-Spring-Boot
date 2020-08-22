a70ea20ba9d8d2c7d47c71926d35855cafe2204f
furk2sahin <furkannsahin.7@gmail.com>
2020-08-22 17:38:19 +0300

    first commit

---
+++ src/main/java/com/furkansahin/todoList/TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.furkansahin.todoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 1TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}
